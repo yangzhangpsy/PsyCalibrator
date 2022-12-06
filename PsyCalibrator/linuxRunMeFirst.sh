75a851c30a7be11ad243f3a0cb255a5862f68eb7
yangzhangpsy <35324391+yangzhangpsy@users.noreply.github.com>
2022-12-06 19:48:49 +0800

    Update linuxRunMeFirst.sh

--- PsyCalibrator/linuxRunMeFirst.sh
+++ PsyCalibrator/linuxRunMeFirst.sh
@@ -8,9 +8,9 @@ if [ -f "/etc/udev/rules.d/69-cd-sensors.rules"] | [ -f "/usr/lib/udev/rules.d/6
     sudo usermod -a -G colord $USER
 else
     if [ ! -d "/etc/udev/rules.d" ]; then
-        cp -f ./55-Spyder.rules /etc/udev/rules.d/55-Spyder.rules
-    else
         cp -f ./55-Spyder.rules /usr/lib/udev/rules.d/55-Spyder.rules
+    else
+        cp -f ./55-Spyder.rules /etc/udev/rules.d/55-Spyder.rules
     fi
 fi
 
